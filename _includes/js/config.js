c1031024b31ecc87450472421816c6bd59f7f1a1
Kim <kimstp@di.ku.dk>
2026-01-27 19:15:29 +0100

    Adding all relevant datasetKeys.

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -17,6 +17,19 @@ var institutionKeys = [
 ];
 
 var datasetKeys = [
+    'f506ae89-e503-414a-9e4c-fefee5b5a07b', // NHMD Amber
+    'acf5050c-3a41-4345-a660-652cb9462379', // NHMD Biocultural Botany
+    '5b5305ae-35d1-4992-9d1a-f447db84b471', // NHMD Danekrae
+    'e0459621-92c2-40ac-9934-72b3b1384dc3', // NHMD Entomology
+    '8c834f97-c5df-4280-9623-86594979f91a', // NHMD Herpetology
+    '84dbaec2-f762-11e1-a439-00145eb45e9a', // NHMD Ichthyology
+    '58fc397b-eee4-4138-8a5f-50f9dfb00216', // NHMD Ivertebrate Zoology
+    '78b270c5-a5fe-4f1d-b87e-eb0dd7b7ae02', // NHMD Mammalogy
+    '84d13eba-f762-11e1-a439-00145eb45e9a', // NHMD Mycology
+    'e5c5cad9-b987-4bb4-b07b-85488c5fdd80', // NHMD Ornithology
+    '51f8bbe4-36e9-44c2-9f66-eaa5c7017a5c', // NHMD Quaternary Zoology
+    '0d1f9cee-7cb7-4d3a-a8c4-d2ca6edcd23b', // NHMD Vascular Plants
+    '8e1b54db-ce7f-402e-a36e-c1382f0b7b97', // NHMD Vertebrate Paleontology
     'ab38a0f2-fe39-42d8-92e1-97d2e0a5e2f6', // NHMA Entomology
     '6015b708-235e-43d2-aa8e-aced24f57e0a', // NHMA Freshwater
     '8f67f5f4-69ee-4ddb-bd16-cb727d384eda', // NHMA Invertebrates wet
@@ -188,15 +201,15 @@ var siteConfig = {
     "literatureSearch": {
         "scope": {
             "type": "in",
-            "key": "datasetKey",
+            /*"key": "datasetKey",
             "values": datasetKeys
-            /*"key": "publishingOrganizationKey",
+            */
+            "key": "publishingOrganizationKey",
             "values": [
                 "760d5f24-4c04-40da-9646-1b2c935da502",
                 "2e7df380-8356-4533-bcb3-5459e23c794e",
                 "8e1a97a0-3ca8-11d9-8439-b8a03c50a862"
             ]
-            */
         }
     }
 }
