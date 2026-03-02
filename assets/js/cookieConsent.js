6d5542942fc9488a58b53ccafecdbcadaac6d8d9
Henz00 <henzoo.hansen@gmail.com>
2026-03-02 11:18:09 +0100

    cookie buttons are functional, but cookies themselves are not yet implementet. Contact page email has been made clickable with mailto:, frontpage button links can now direct to correct language pages if given the correct link in the i18n pages.

---
+++ assets/js/cookieConsent.js
@@ -0,0 +1,13 @@
+const banner = document.getElementById('cookie-banner');
+const btnAccept = document.getElementById('cookie-accept');
+const btnReject = document.getElementById('cookie-reject');
+
+function hideBanner() {
+    banner.style.display = 'none';
+    // Implement here what happens when the user declines cookies
+}
+
+function showBanner() { 
+    banner.style.display = 'none';
+    // Implement here what happens when the user accepts cookies
+}
